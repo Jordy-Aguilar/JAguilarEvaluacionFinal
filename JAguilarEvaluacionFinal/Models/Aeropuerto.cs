61c2156b868d068f872dcf89f9d4e9b41087e88a
Jordy-Aguilar <jeay022@gmail.com>
2025-01-24 07:33:42 -0500

    cree carpeta models y clase aeropuerto

---
+++ JAguilarEvaluacionFinal/Models/Aeropuerto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAguilarEvaluacionFinal.Models
+{
+    public class Aeropuerto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Pais {  get; set; }
+        public double latitud { get; set; }
+        public double longitud { get; set; }
+        public string correo { get; set; }
+        public string usuario { get; set; }
+    }
+}

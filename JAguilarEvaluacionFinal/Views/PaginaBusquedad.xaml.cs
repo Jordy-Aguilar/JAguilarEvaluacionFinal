3587c57a7aaa0ddfb67b7572fea82250f816a52e
Jordy-Aguilar <jeay022@gmail.com>
2025-01-24 08:11:04 -0500

    modifique xaml.cs de pagina busquedad

--- JAguilarEvaluacionFinal/Views/PaginaBusquedad.xaml.cs
+++ JAguilarEvaluacionFinal/Views/PaginaBusquedad.xaml.cs
@@ -1,9 +1,43 @@
+using JAguilarEvaluacionFinal.Servicios;
+using JAguilarEvaluacionFinal.Models;
+ 
 namespace JAguilarEvaluacionFinal.Views;
 
-public partial class PaginaBusquedad : ContentPage
+public partial class PaginaBusqueda : ContentPage
 {
-	public PaginaBusquedad()
-	{
-		InitializeComponent();
-	}
+    private ServicioAeropuerto servicio;
+
+    public PaginaBusqueda()
+    {
+        InitializeComponent();
+        servicio = new ServicioAeropuerto();
+    }
+
+    private async void OnBuscarClicked(object sender, EventArgs e)
+    {
+        var nombre = NombreAeropuerto.Text;
+
+        if (!string.IsNullOrWhiteSpace(nombre))
+        {
+            Resultado.Text = "Buscando...";
+            var aeropuerto = await servicio.BuscarAeropuerto(nombre);
+
+            if (aeropuerto != null)
+            {
+                Resultado.Text = $"Nombre: {aeropuerto.Name}\n" +
+                $"País: {aeropuerto.Country}\n" +
+                                                 $"IATA: {aeropuerto.Iata}\n" +
+                                                 $"Latitud: {aeropuerto.Latitude}\n" +
+                                                 $"Longitud: {aeropuerto.Longitude}";
+            }
+            else
+            {
+                Resultado.Text = "No se encontró ningún aeropuerto con ese nombre.";
+            }
+        }
+        else
+        {
+            Resultado.Text = "Ingrese un nombre válido.";
+        }
+    }
 }
